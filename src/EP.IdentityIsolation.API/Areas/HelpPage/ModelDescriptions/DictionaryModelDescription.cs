c4a539db8b3aae0445f486f65ae5c94d149396d7
Eduardo Bellinat <eduardo@bellinat.com>
2015-12-17 10:11:15 -0200

    Adicionando projeto Web API

---
+++ src/EP.IdentityIsolation.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EP.IdentityIsolation.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/EP.IdentityIsolation.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace EP.IdentityIsolation.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
