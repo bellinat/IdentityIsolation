83b2bce5b1b2b525c56b739c9290add82edc4833
Eduardo Bellinat <eduardo@bellinat.com>
2016-01-28 07:52:54 -0200

    Teste

--- src/EP.IdentityIsolation.API/App_Start/Startup.Auth.cs
+++ src/EP.IdentityIsolation.API/App_Start/Startup.Auth.cs
@@ -27,6 +27,7 @@ namespace EP.IdentityIsolation.API
             //app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationUserManager>());
             //app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationSignInManager>());
 
+
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
